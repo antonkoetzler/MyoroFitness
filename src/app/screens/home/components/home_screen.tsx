24e3532659a65dfdb7de922ba83fa097106609e9
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-03-12 21:53:04 -0300

    Formatting files

--- src/app/screens/home/components/home_screen.tsx
+++ src/app/screens/home/components/home_screen.tsx
@@ -1,10 +1,8 @@
-import { View } from "react-native";
+import { View } from 'react-native';
 
 /**
  * Main screen of the application.
  */
 export default function HomeScreen() {
-  return (
-    <View className={'bg-primary'}>Lets start</View>
-  );
+  return <View className={'bg-primary'}>Lets start</View>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,3 @@ export const theme = {
   },
 };
 export const plugins = [];
-
